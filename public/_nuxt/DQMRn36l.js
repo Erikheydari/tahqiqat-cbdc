eb272f915900ec5f53ffb0e0212828d251b8361c
erikheydari <erikheydari@gmail.com>
2025-07-20 16:04:22 +0330

    first

---
+++ public/_nuxt/DQMRn36l.js
@@ -0,0 +1,1 @@
+import{g as e,j as n}from"./CrnylogF.js";const u=e({__name:"index",setup(t){return n().push("/accounts"),(s,o)=>null}});export{u as default};
---
+++ server/chunks/build/client.manifest.mjs
@@ -0,0 +1,419 @@
+const client_manifest = {
+  "../node_modules/nuxt/dist/app/components/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CjlYZRub.js",
+    "name": "error-404",
+    "src": "../node_modules/nuxt/dist/app/components/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js",
+      "_DlAUqK2U.js",
+      "_Dx8nPXpC.js"
+    ],
+    "css": [
+      "error-404.DlVPZ4GE.css"
+    ]
+  },
+  "error-404.DlVPZ4GE.css": {
+    "file": "error-404.DlVPZ4GE.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "../node_modules/nuxt/dist/app/components/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CSRoelET.js",
+    "name": "error-500",
+    "src": "../node_modules/nuxt/dist/app/components/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_DlAUqK2U.js",
+      "_Dx8nPXpC.js",
+      "../node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "error-500.DjyirMQI.css"
+    ]
+  },
+  "error-500.DjyirMQI.css": {
+    "file": "error-500.DjyirMQI.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "../node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CrnylogF.js",
+    "name": "entry",
+    "src": "../node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "layouts/default.vue",
+      "layouts/guest.vue",
+      "../node_modules/nuxt/dist/app/components/error-404.vue",
+      "../node_modules/nuxt/dist/app/components/error-500.vue"
+    ],
+    "css": [
+      "entry.Bcx81gbV.css"
+    ],
+    "assets": [
+      "Vazirmatn-Thin.DdUiS1cj.woff2",
+      "Vazirmatn-ExtraLight.DNfybfsL.woff2",
+      "Vazirmatn-Light.Byrs-DlH.woff2",
+      "Vazirmatn-Regular.CtGL0YI-.woff2",
+      "Vazirmatn-Medium.D86t5Axy.woff2",
+      "Vazirmatn-SemiBold.26ZP1ILp.woff2",
+      "Vazirmatn-Bold._OhAJEdh.woff2",
+      "Vazirmatn-ExtraBold.DCjt2qDm.woff2",
+      "Vazirmatn-Black.DA1YqYMq.woff2",
+      "Vazirmatn_wght_.BeciDpKm.woff2"
+    ]
+  },
+  "entry.Bcx81gbV.css": {
+    "file": "entry.Bcx81gbV.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "Vazirmatn-Thin.DdUiS1cj.woff2": {
+    "file": "Vazirmatn-Thin.DdUiS1cj.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "Vazirmatn-ExtraLight.DNfybfsL.woff2": {
+    "file": "Vazirmatn-ExtraLight.DNfybfsL.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "Vazirmatn-Light.Byrs-DlH.woff2": {
+    "file": "Vazirmatn-Light.Byrs-DlH.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "Vazirmatn-Regular.CtGL0YI-.woff2": {
+    "file": "Vazirmatn-Regular.CtGL0YI-.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "Vazirmatn-Medium.D86t5Axy.woff2": {
+    "file": "Vazirmatn-Medium.D86t5Axy.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "Vazirmatn-SemiBold.26ZP1ILp.woff2": {
+    "file": "Vazirmatn-SemiBold.26ZP1ILp.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "Vazirmatn-Bold._OhAJEdh.woff2": {
+    "file": "Vazirmatn-Bold._OhAJEdh.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "Vazirmatn-ExtraBold.DCjt2qDm.woff2": {
+    "file": "Vazirmatn-ExtraBold.DCjt2qDm.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "Vazirmatn-Black.DA1YqYMq.woff2": {
+    "file": "Vazirmatn-Black.DA1YqYMq.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "Vazirmatn_wght_.BeciDpKm.woff2": {
+    "file": "Vazirmatn_wght_.BeciDpKm.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "_B5973HA4.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "B5973HA4.js",
+    "name": "index",
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js",
+      "_DlAUqK2U.js",
+      "_DVd454Ti.js",
+      "_iMMoY53r.js"
+    ],
+    "css": [
+      "index.i9zz5_9y.css"
+    ],
+    "assets": [
+      "Vazirmatn-Thin.DdUiS1cj.woff2",
+      "Vazirmatn-ExtraLight.DNfybfsL.woff2",
+      "Vazirmatn-Light.Byrs-DlH.woff2",
+      "Vazirmatn-Regular.CtGL0YI-.woff2",
+      "Vazirmatn-Medium.D86t5Axy.woff2",
+      "Vazirmatn-SemiBold.26ZP1ILp.woff2",
+      "Vazirmatn-Bold._OhAJEdh.woff2",
+      "Vazirmatn-ExtraBold.DCjt2qDm.woff2",
+      "Vazirmatn-Black.DA1YqYMq.woff2",
+      "Vazirmatn_wght_.BeciDpKm.woff2"
+    ]
+  },
+  "index.i9zz5_9y.css": {
+    "file": "index.i9zz5_9y.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "_BlOgaIcf.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BlOgaIcf.js",
+    "name": "index",
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_CuAwSxlY.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CuAwSxlY.js",
+    "name": "rotate-cw",
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_DVd454Ti.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DVd454Ti.js",
+    "name": "CopyButton.vue",
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js",
+      "_BlOgaIcf.js"
+    ]
+  },
+  "_DlAUqK2U.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DlAUqK2U.js",
+    "name": "_plugin-vue_export-helper"
+  },
+  "_Dx8nPXpC.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Dx8nPXpC.js",
+    "name": "composables",
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_iMMoY53r.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "iMMoY53r.js",
+    "name": "apiStore",
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_index.i9zz5_9y.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "index.i9zz5_9y.css",
+    "src": "_index.i9zz5_9y.css"
+  },
+  "assets/fonts/web/Vazirmatn-Black.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Vazirmatn-Black.DA1YqYMq.woff2",
+    "src": "assets/fonts/web/Vazirmatn-Black.woff2"
+  },
+  "assets/fonts/web/Vazirmatn-Bold.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Vazirmatn-Bold._OhAJEdh.woff2",
+    "src": "assets/fonts/web/Vazirmatn-Bold.woff2"
+  },
+  "assets/fonts/web/Vazirmatn-ExtraBold.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Vazirmatn-ExtraBold.DCjt2qDm.woff2",
+    "src": "assets/fonts/web/Vazirmatn-ExtraBold.woff2"
+  },
+  "assets/fonts/web/Vazirmatn-ExtraLight.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Vazirmatn-ExtraLight.DNfybfsL.woff2",
+    "src": "assets/fonts/web/Vazirmatn-ExtraLight.woff2"
+  },
+  "assets/fonts/web/Vazirmatn-Light.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Vazirmatn-Light.Byrs-DlH.woff2",
+    "src": "assets/fonts/web/Vazirmatn-Light.woff2"
+  },
+  "assets/fonts/web/Vazirmatn-Medium.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Vazirmatn-Medium.D86t5Axy.woff2",
+    "src": "assets/fonts/web/Vazirmatn-Medium.woff2"
+  },
+  "assets/fonts/web/Vazirmatn-Regular.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Vazirmatn-Regular.CtGL0YI-.woff2",
+    "src": "assets/fonts/web/Vazirmatn-Regular.woff2"
+  },
+  "assets/fonts/web/Vazirmatn-SemiBold.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Vazirmatn-SemiBold.26ZP1ILp.woff2",
+    "src": "assets/fonts/web/Vazirmatn-SemiBold.woff2"
+  },
+  "assets/fonts/web/Vazirmatn-Thin.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Vazirmatn-Thin.DdUiS1cj.woff2",
+    "src": "assets/fonts/web/Vazirmatn-Thin.woff2"
+  },
+  "assets/fonts/web/Vazirmatn[wght].woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Vazirmatn_wght_.BeciDpKm.woff2",
+    "src": "assets/fonts/web/Vazirmatn[wght].woff2"
+  },
+  "layouts/default.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Btp5annK.js",
+    "name": "default",
+    "src": "layouts/default.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js",
+      "_iMMoY53r.js",
+      "_BlOgaIcf.js"
+    ]
+  },
+  "layouts/guest.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DVQEvzYM.js",
+    "name": "guest",
+    "src": "layouts/guest.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_DlAUqK2U.js",
+      "../node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "pages/accounts.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "B0sMglTG.js",
+    "name": "accounts",
+    "src": "pages/accounts.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js",
+      "_iMMoY53r.js",
+      "_B5973HA4.js",
+      "_DVd454Ti.js",
+      "_CuAwSxlY.js",
+      "_DlAUqK2U.js",
+      "_BlOgaIcf.js"
+    ]
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DQMRn36l.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "pages/payments.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BrL28C0m.js",
+    "name": "payments",
+    "src": "pages/payments.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js",
+      "_iMMoY53r.js",
+      "_B5973HA4.js",
+      "_DVd454Ti.js",
+      "_CuAwSxlY.js",
+      "_DlAUqK2U.js",
+      "_BlOgaIcf.js"
+    ]
+  },
+  "pages/receives.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Yh_6CaIj.js",
+    "name": "receives",
+    "src": "pages/receives.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js",
+      "_iMMoY53r.js",
+      "_B5973HA4.js",
+      "_DlAUqK2U.js",
+      "_DVd454Ti.js",
+      "_BlOgaIcf.js"
+    ]
+  },
+  "pages/token.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "WCZv2k4O.js",
+    "name": "token",
+    "src": "pages/token.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_DVd454Ti.js",
+      "../node_modules/nuxt/dist/app/entry.js",
+      "_BlOgaIcf.js"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
